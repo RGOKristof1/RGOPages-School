6968d8e1f8b8c4e1fc0d3d25e6a02477452bbe4b
Rendszergazda <rendszergazda@blathy.info>
2026-02-23 10:40:25 +0100

    oriasi progress on wizgaeme

--- wiza-game/wiza-game-source/scripts/main.js
+++ wiza-game/wiza-game-source/scripts/main.js
@@ -1,11 +1,7 @@
 function generateGrid(gridSize) {
   document.getElementById("playGround").innerHTML = ""
   document.getElementById("playGround").style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`
-  document.getElementById("playGround").style.width = `${(gridSize*2)-10}rem`
-  document.getElementById("playGround").style.height = `${(gridSize*2)-10}rem`
   for (let i = 0; i < gridSize**2; i++) {
-          const child = document.createElement("div")
-          document.getElementById("playGround").appendChild(child)
+          document.getElementById("playGround").innerHTML += `<div id="${i}"></div>`
       }
-    
 }
