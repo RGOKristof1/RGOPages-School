d31956c98c2501036b2184a5a3d78f3b8299dce5
Rendszergazda <rendszergazda@blathy.info>
2026-03-19 14:06:32 +0100

    nem megy de kinda jo helyen tartok mar  astar gameben

--- stargame/game.js
+++ stargame/game.js
@@ -1,12 +1,12 @@
 
-let gameTime = 60;
+let gameTime = 0;
 let stars = []
+let timeEnded = false
+let starsLeft = true
+let timerStopped = false
+
 function randomBetween(min, max) {
     return Math.floor(Math.random() * (max + 1 - min) + min);
-}
-
-function endGame(state ) {
-
 }
 
 function catchStar(starID) {
@@ -18,42 +18,85 @@ function catchStar(starID) {
     }
 
     console.log("removed", starID);
+    
     if (stars.length == 0) {
-        endGame("win")
+        starsLeft = false
+    } else {
+        starsLeft = true
     }
 }
 
+function endGame(){
+    document.getElementById("io").disabled = false;
+    document.getElementById("io").disabled = false;
+}
 
 function startTimer(time) {
-  const interval = setInterval(() => {
+    timeEnded = false
+    timerStopped = false
+    const interval = setInterval(() => {
     time--;
     document.getElementById("startButton").innerText = time
 
     if (time <= 0) {
       clearInterval(interval);
-      endGame("lose")
+      timeEnded = true
+    } else if (timerStopped) {
+        clearInterval(interval);
+        time = 0
+        document.getElementById("startButton").innerText = "Start";
+        document.getElementById("startButton").disabled = false;
+        endGame()
     }
   }, 1000);
 }
 
 function moveStars(){
     for (let i = 0; i < stars.length; i++) {
-        document.getElementById()
-        
+        let randomPosX = randomBetween(5,95)
+        let randomPosY = randomBetween(5,95)
+        document.getElementById(`${stars[i]}`).style.top = `${randomPosY}%`
+        document.getElementById(`${stars[i]}`).style.left = `${randomPosX}%`
     }
 }
 
 function spawnStars(starCount) {
     for (let i = 0; i < starCount; i++) {
         let starID = `${i+1}id`
-        let randomPosX = randomBetween(5,95)
-        let randomPosY = randomBetween(5,95)
-        document.getElementById("playground").innerHTML += `<div class="star" onClick="catchStar('${starID}')" id="${starID}" style="top: ${randomPosY}%; left: ${randomPosX}%;">🌕</div>`
+        let starSkin = document.getElementById("io").value || "🌕"
+        document.getElementById("playground").innerHTML += `<div class="star" onClick="catchStar('${starID}')" id="${starID}">${starSkin}</div>`
         stars.push(starID)
     }
+    console.log(stars)
 }
+
+function stopTimer() {
+    timerStopped = true
+}
+
+
 function startGame() {
+    gameTime = 60;
+    stars = []
+    timeEnded = false
+    timerStopped = false
     document.getElementById("startButton").disabled = true;
+    document.getElementById("io").disabled = true;
     startTimer(gameTime)
     spawnStars(10)
+    function wait(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+      }
+      
+      async function gameLoop() {
+        while (!timeEnded || !starsLeft) {
+          moveStars();
+
+          await wait(1000);
+        }
+        stopTimer()
+      }
+      
+      gameLoop();
+      
 }
